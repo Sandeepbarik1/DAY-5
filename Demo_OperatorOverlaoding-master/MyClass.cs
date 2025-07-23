c7493be40c495bc9bdcb8d61186988ef9341b1a1
Sandeepbarik1 <sandeepbarik785@gmail.com>
2025-07-23 12:12:44 +0530

    Add files via upload

---
+++ Demo_OperatorOverlaoding-master/MyClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OperatorOverlaoding
+{
+    internal class MyClass
+    {
+        private string _name;// here i aking name of the class MyClass
+        public MyClass(string name) // creating constrcutor
+        {
+            _name = name;
+        }
+
+        // Overloading the + operator
+        public static MyClass operator +(MyClass a, MyClass b)//here we are overaloding the + operator to join two strings
+        {
+            // Concatenating the names of the two MyClass instances
+            //here reurntype is MyClass and we are returning a new instance of MyClass with the concatenated name
+            return new MyClass(a._name + " " + b._name);//here we are concatenating two string 
+        }
+        //overloading == operator
+        public static bool operator ==(MyClass a, MyClass b) // here we are overloading the == operator to compare two strings
+        {
+            // Checking if the names of the two MyClass instances are equal
+            //here return types is bool and we are returning true if both strings are equal or false if they are not equal
+            return a._name == b._name; //here we are checking if both strings are equal
+        }
+        //overloading != operator
+        public static bool operator !=(MyClass a, MyClass b) // here we are overloading the != operator to compare two strings
+        {
+            // Checking if the names of the two MyClass instances are not equal
+            //here return types is bool and we are returning true if both strings are not equal or false if they are equal
+
+            return a._name != b._name; //here we are checking if both strings are not equal
+        }
+        // Overriding the ToString method to return the name of the MyClass instance
+        public override string ToString() // here we are overriding the ToString method to return the name of the MyClass instance without using the abstract keyword
+        {
+            return _name; //here we are returning the name of the MyClass instance
+            //objective of overriding ToString method is to provide a string representation of the MyClass instance without invoking ToString method explicitly
+        }
+        // Overriding the Equals method to compare two MyClass instances
+        public override bool Equals(object obj) // here we are overriding the Equals method to compare two MyClass instances
+        {
+            if (obj is MyClass other) // checking if the object is of type MyClass
+            {
+                return _name == other._name; //here we are checking if both strings are equal
+            }
+            return false; //if the object is not of type MyClass, we return false
+        }
+        // Overriding the GetHashCode method to return the hash code of the name of the MyClass instance
+        public override int GetHashCode() // here we are overriding the GetHashCode method to return the hash code of the name of the MyClass instance
+        {
+            return _name.GetHashCode(); //here we are returning the hash code of the name of the MyClass instance
+        }
+        public static MyClass CreateInstance(string name) // here we are creating a static method to create an instance of MyClass
+        {
+            return new MyClass(name); //here we are returning a new instance of MyClass with the given name
+        }
+
+        //While creating an Abstract Method here in overlaoding the + operator, we are not using the abstract keyword because we are not creating an abstract class.
+       
+
+    }
+}
---
+++ Demo_OperatorOverlaoding-master/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OperatorOverlaoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OPerator overloading includes giving addittional meaning to operapors so that they can work on different data types apart from int");
+            //Step 1: Create a class
+            //Step 2: Create a method that overloads an operator(in this case, the + operator)
+            //Step 3: Create an instance of the class and use the overloaded operator
+            //Step 4: Observe the output
+            // Defination : with the help of operator overloading, we can give additional meaning to operators so that they can work on different data types apart from int, float, double, etc.
+            //Use: // Operator overloading is used to define custom behavior for operators when applied to user-defined types, allowing for more intuitive and readable code.
+            
+            //Creating objects of MyClass
+            MyClass obj1 = new MyClass("Anurag kashyap");
+            MyClass obj2 = new MyClass("Anurag basu");
+            //testing operator overloading with above objects 
+            MyClass obj3 = obj1 + obj2; // using overloaded + operator to concatenate names
+            Console.WriteLine("Using overloaded + operator: " + obj3); // Output: Anurag Kumar
+            //testing other operator overloading with same objects
+
+            bool areEqual = obj1 == obj2; // using overloaded == operator to check equality
+            Console.WriteLine("Using overloaded == operator: " + areEqual); // Output: False
+
+            bool areNotEqual = obj1 != obj2; // using overloaded != operator to check inequality
+            Console.WriteLine("Using overloaded != operator: " + areNotEqual); // Output: True
+            // Overriding ToString method to display the name of the MyClass instance
+            Console.WriteLine("Using overridden ToString method: " + obj1); // Output: Anurag Kashyap
+            Console.WriteLine("Using overridden ToString method: " + obj2); // Output: Anurag Basu
+
+            // Overriding Equals method to compare two MyClass instances
+            bool isEqual = obj1.Equals(obj2); // using overridden Equals method to compare instances
+            Console.WriteLine("Using overridden Equals method: " + isEqual); // Output: False
+
+            // Overriding GetHashCode method to return the hash code of the name of the MyClass instance
+            int hashCode = obj1.GetHashCode(); // using overridden GetHashCode method to get the hash code
+            int hashCode2 = obj2.GetHashCode(); // using overridden GetHashCode method to get the hash code of another instance
+            Console.WriteLine("Using overridden GetHashCode method for firs object : " + hashCode); // Output: Hash code of the name
+            Console.WriteLine("Using overridden GetHashCode method for second object : " + hashCode2); // Output: Hash code of the name
+
+
+        }
+    }
+}
